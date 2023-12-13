280dfa42b6cc5a70d057b2547593d506bbaba8fb
Minamino9795 <minamino9795@gmail.com>
2023-12-13 09:57:37 +0700

    layout

---
+++ src/layouts/Master.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Master(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Master;
---
+++ src/pages/CheckoutSuccess.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CheckoutSuccess(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CheckoutSuccess;
